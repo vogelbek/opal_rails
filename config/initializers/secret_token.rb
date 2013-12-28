2cb17529d111cbe0e8e74273143a485002ec90b7
Adam Hendricksen <adam@nird.us>
2013-12-28 13:58:28 -0800

    New playground with opal for JS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpalRailsPlayground::Application.config.secret_key_base = '54844260f5391d55257cd29c94895c8cdd26e18ed9d13c3db28d759ba5f62fcf2a0e282f3cd9007317372e0c95c894b6beb31e4f523065451b40c05e41cac5d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpalRailsPlayground::Application.config.session_store :cookie_store, key: '_opal_rails_playground_session'
